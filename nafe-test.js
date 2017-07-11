5f4ef60346b76a9f94e8a79b67b9ac467612036d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-11 11:04:24 +0800

    Updated server-side test.

--- nafe-test.js
+++ nafe-test.js
@@ -2,9 +2,11 @@ const assert = require( "assert" );
 const nafe = require( "./nafe.js" );
 
 assert.equal( nafe( Array.prototype.push ), true, "should be true" );
-assert.equal( nafe( Object.prototype.toString ), true, "should be true" );
-assert.equal( nafe( Date.prototype.getDate ), true, "should be true" );
+
+assert.equal( nafe( Error ), true, "should return true" );
 
 assert.equal( nafe( function( ){ } ), false, "should be false" );
+
+assert.equal( nafe( ( ) => { } ), false, "should be true" );
 
 console.log( "ok" );
